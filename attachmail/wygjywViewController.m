3d6b65a3bd380b74aafd78feb48d16c2c9adb56b
wygjyw <wygjyw@gmail.com>
2013-08-20 23:59:21 +0800

    final verison

--- attachmail/wygjywViewController.m
+++ attachmail/wygjywViewController.m
@@ -59,14 +59,59 @@
     MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
     mailController.mailComposeDelegate = self;
     [mailController setSubject:emailTitle];
-    [mailController setMessageBody:messageBody isHTML:NO];
+    [mailController setMessageBody:messageBody isHTML:YES];
     [mailController setToRecipients:toRecipents];
     
+    // Determine the file name and extension
+    NSArray *filepart = [file componentsSeparatedByString:@"."];
+    NSString *filename = [filepart objectAtIndex:0];
+    NSString *extension = [filepart objectAtIndex:1];
+    
+    // Get the resource path and read the file using NSData
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:filename ofType:extension];
+    NSData *fileData = [NSData dataWithContentsOfFile:filePath];
+    
+    // Determine the MIME type
+    NSString *mimeType;
+    if ([extension isEqualToString:@"jpg"]) {
+        mimeType = @"image/jpeg";
+    } else if ([extension isEqualToString:@"png"]) {
+        mimeType = @"image/png";
+    } else if ([extension isEqualToString:@"doc"]) {
+        mimeType = @"application/msword";
+    } else if ([extension isEqualToString:@"ppt"]) {
+        mimeType = @"application/vnd.ms-powerpoint";
+    } else if ([extension isEqualToString:@"html"]) {
+        mimeType = @"text/html";
+    } else if ([extension isEqualToString:@"pdf"]) {
+        mimeType = @"application/pdf";
+    }
+    
+    // Add attachment
+    [mailController addAttachmentData:fileData mimeType:mimeType fileName:filename];
+
     [self presentViewController:mailController animated:YES completion:nil];
 }
 
 - (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
 {
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
