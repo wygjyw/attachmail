7d53403c337507ed9f33021ed49542609b9ea73e
wygjyw <wygjyw@gmail.com>
2013-08-20 22:24:40 +0800

    init version

---
+++ attachmail/wygjywAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  wygjywAppDelegate.h
+//  attachmail
+//
+//  Created by Wyg Jyw on 8/20/13.
+//  Copyright (c) 2013 wygjyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wygjywAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ attachmail/wygjywViewController.h
@@ -0,0 +1,13 @@
+//
+//  wygjywViewController.h
+//  attachmail
+//
+//  Created by Wyg Jyw on 8/20/13.
+//  Copyright (c) 2013 wygjyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wygjywViewController : UIViewController
+
+@end
