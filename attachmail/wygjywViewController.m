798857021ad1d46e98b95d2079bc1265c3951174
wygjyw <wygjyw@gmail.com>
2013-08-20 23:41:49 +0800

    simple send mail feature

--- attachmail/wygjywViewController.m
+++ attachmail/wygjywViewController.m
@@ -8,8 +8,9 @@
 
 #import "wygjywViewController.h"
 #import "wygjywViewControllerCell.h"
+#import <MessageUI/MessageUI.h>
 
-@interface wygjywViewController ()
+@interface wygjywViewController () <MFMailComposeViewControllerDelegate>
 
 @property (strong, nonatomic) NSArray *fileList;
 
@@ -41,6 +42,32 @@
     cell.attachFileName.text = _fileList[indexPath.row];
     cell.attchFileIcon.image = [UIImage imageNamed:[NSString stringWithFormat:@"icon%d", indexPath.row]];
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *selectedFile = [_fileList objectAtIndex:indexPath.row];
+    [self sendMail:selectedFile];
+}
+
+-(void)sendMail:(NSString *)file
+{
+    NSString *emailTitle = @"Great Photo and Doc";
+    NSString *messageBody = @"Hey, check this out!";
+    NSArray *toRecipents = [NSArray arrayWithObject:@"wygjyw@gmail.com"];
+
+    MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
+    mailController.mailComposeDelegate = self;
+    [mailController setSubject:emailTitle];
+    [mailController setMessageBody:messageBody isHTML:NO];
+    [mailController setToRecipients:toRecipents];
+    
+    [self presentViewController:mailController animated:YES completion:nil];
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 
